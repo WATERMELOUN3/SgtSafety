e3d6394e83d97d71a54a51234799613ce6027bd7
Alexis GÉLIN--ANDRIEU <alexis.gelin@outlook.fr>
2019-04-24 12:25:47 +0200

    Problèmes de types résolu

--- SgtSafety/NXTEnvironment/NXTCase.cs
+++ SgtSafety/NXTEnvironment/NXTCase.cs
@@ -1,6 +1,7 @@
+﻿using Microsoft.Xna.Framework;
+using SgtSafety.Types;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -90,6 +91,200 @@ namespace SgtSafety.NXTEnvironment
         {
             NXTCase c = new NXTCase(this.typeCase, this.orientation);
             return c;
+        }
+        //Retourne le point correspondant � un d�placement dans un virage selon la direction du vehicule
+        private Point goThroughVirage(Point direction)
+        {
+            switch (this.orientation)
+            {
+                case Orientation.BOTTOM:
+                    if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.TOP;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.LEFT;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.LEFT:
+                    if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.BOTTOM;
+                    else if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.LEFT;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.RIGHT:
+                    if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.TOP;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.RIGHT;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.TOP:
+                    if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.BOTTOM;
+                    else if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.RIGHT;
+                    return NXTVehicule.ERROR;
+            }
+
+            return NXTVehicule.ERROR;
+        }
+
+        //Retourne le point correspondant � un d�placement STRAIGHT dans la case
+        private Point goStraight(Point direction)
+        {
+            switch (this.typeCase)
+            {
+                case Case.STRAIGHT:
+                    return direction;
+                case Case.VIRAGE:
+                    return goThroughVirage(direction);
+                case Case.INTERSECTION:
+                    return NXTVehicule.ERROR;
+                default:
+                    return NXTVehicule.ERROR;
+            }
+        }
+
+        //Retourne le point correspondant � un d�placement UTURN
+        private Point goUTurn(Point direction)
+        {
+            Point newDirection = NXTVehicule.oppositeDirection(direction);
+            switch (this.typeCase)
+            {
+                case Case.STRAIGHT:
+                    return newDirection;
+                case Case.VIRAGE:
+                    return goThroughVirage(newDirection);
+                case Case.INTERSECTION:
+                    return NXTVehicule.ERROR;
+                default:
+                    return NXTVehicule.ERROR;
+            }
+        }
+
+        //Retourne le point correspondant � une intersection prise � gauche
+        private Point goThroughInterLeft(Point direction)
+        {
+            switch (this.orientation)
+            {
+                case Orientation.BOTTOM:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.BOTTOM;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.RIGHT;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.LEFT:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.BOTTOM;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.TOP;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.RIGHT:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.TOP;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.RIGHT;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.BOTTOM;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.TOP:
+                    if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.RIGHT;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.TOP;
+                    return NXTVehicule.ERROR;
+            }
+
+            return NXTVehicule.ERROR;
+        }
+
+        //Retourne le point correspondant � une intersection prise � droite
+        private Point goThroughInterRight(Point direction)
+        {
+            switch (this.orientation)
+            {
+                case Orientation.BOTTOM:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.RIGHT;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.BOTTOM;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.LEFT:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.TOP;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.BOTTOM;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.RIGHT:
+                    if (direction == NXTVehicule.TOP)
+                        return NXTVehicule.RIGHT;
+                    else if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.BOTTOM;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.TOP;
+                    return NXTVehicule.ERROR;
+
+                case Orientation.TOP:
+                    if (direction == NXTVehicule.BOTTOM)
+                        return NXTVehicule.LEFT;
+                    else if (direction == NXTVehicule.LEFT)
+                        return NXTVehicule.TOP;
+                    else if (direction == NXTVehicule.RIGHT)
+                        return NXTVehicule.RIGHT;
+                    return NXTVehicule.ERROR;
+            }
+
+            return NXTVehicule.ERROR;
+        }
+
+        //Retourne le point correspondant à une intersection prise selon le cot� donn� en param�tre
+        private Point turnInter(Point direction, char side)
+        {
+            switch (this.typeCase)
+            {
+                case Case.STRAIGHT:
+                    return NXTVehicule.ERROR;
+                case Case.VIRAGE:
+                    return NXTVehicule.ERROR;
+                case Case.INTERSECTION:
+                    if (side == NXTMovement.INTER_LEFT)
+                        return goThroughInterLeft(direction);
+                    else if (side == NXTMovement.INTER_RIGHT)
+                        return goThroughInterRight(direction);
+                    return NXTVehicule.ERROR;
+                default:
+                    return NXTVehicule.ERROR;
+            }
+        }
+
+        //Retourne le point correspondant au déplacement effectu� dans la case selon l'action donn�e en param�tre
+        public Point goThrough(NXTAction action, Point direction)
+        {
+            char movement = action.Movement;
+
+            if (movement == NXTMovement.STRAIGHT)
+                return goStraight(direction);
+            else if (movement == NXTMovement.INTER_LEFT || movement == NXTMovement.INTER_RIGHT)
+                return turnInter(direction, movement);
+            else if (movement == NXTMovement.UTURN)
+                return goUTurn(direction);
+
+            return new Point(1, 1);
         }
     }
 }
